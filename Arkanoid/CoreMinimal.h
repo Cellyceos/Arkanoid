ebdfb9fb491afbc59e97f6066fdcb139421176d7
Cellyceos <brakir@gmail.com>
2021-02-28 13:31:23 +0300

    Typo fixes

--- Arkanoid/CoreMinimal.h
+++ Arkanoid/CoreMinimal.h
@@ -20,7 +20,7 @@ using uint32 = std::uint32_t;
 using uint64 = std::uint64_t;
 
 template<class T> using UniquePtr = std::unique_ptr<T>;
-template<class T> using ShatedPtr = std::shared_ptr<T>;
+template<class T> using SharedPtr = std::shared_ptr<T>;
 template<class T> using WeakPtr = std::weak_ptr<T>;
 
 using String = std::string;
@@ -30,4 +30,4 @@ template <class T, class... Types>
 UniquePtr<T> MakeUnique(Types&&... Args) { return std::make_unique<T>(std::forward<Types>(Args)...); }
 
 template <class T, class... Types>
-ShatedPtr<T> MakeShared(Types&&... Args) { return std::make_shared<T>(std::forward(Args)...); }
+SharedPtr<T> MakeShared(Types&&... Args) { return std::make_shared<T>(std::forward<Types>(Args)...); }
