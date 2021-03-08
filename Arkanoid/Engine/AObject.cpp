ca23268bc133a5e9e648c608ffb031078f21189f
Cellyceos <brakir@gmail.com>
2021-03-08 14:15:11 +0300

    Delete AObject.cpp

--- Arkanoid/Engine/AObject.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  AObject.cpp
-//  Base Class for All object in the game.
-//
-//  Created by Kirill Bravichev on 03/01/2021.
-//  Copyright (c) 2021 Cellyceos. All rights reserved.
-//
-
-#include "AObject.h"
